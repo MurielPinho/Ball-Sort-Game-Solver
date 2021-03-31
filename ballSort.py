1de5c4c34372ddab2f06c7f364625ccb9e567c38
Muriel Pinho <muriel.apinho@gmail.com>
2021-03-31 20:27:30 +0100

    levels loading done

--- ballSort.py
+++ ballSort.py
@@ -5,45 +5,52 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+levels = [
+    [2,4,[[2,1,2,1],[1,2,1,2],[],[]]],
+    [2,4,[[2,2,2,1],[1,1,1,2],[],[]]],
+    [2,4,[[],[2,1,2,1],[1,2,1,2],[]]],
+    [2,4,[[2,1,2,1],[],[1,2,1,2],[]]]]
 
-arr1=[2,1,2,1]
-arr2=[1,2,1,2]
-arr3 = []
-arr4 = []
-n=2
-m=4
-arrTotal=[arr1,arr2,arr3,arr4]
-completed = [0]*m
+class Game:
+    def __init__(self,level):
+        self.n=levels[level][0]
+        self.m=levels[level][1]
+        #self.arrTotal=[self.arr1,self.arr2,self.arr3,self.arr4]
+        self.arrTotal=levels[level][2]
+        self.completed = [0]*self.m
+        self.tubesArray = [0]*self.m
 
-def fillCompleted(col):
-    if checkCompleted(col):
-        completed[col] = 1
+    def fillCompleted(self,col):
+        if self.checkCompleted(col):
+            self.completed[col] = 1
 
-def checkCompleted(col):
-    if len(set(arrTotal[col])) == 1 and len(arrTotal[col]) == m:
-        return True
-    else: return False
-
-def validMove(fromCol,toCol):
-    if len(arrTotal[fromCol]) > 0 and len(arrTotal[toCol]) < m and fromCol != toCol and not(completed[fromCol]):
-        if len(arrTotal[toCol]) == 0:
-            return True
-        elif arrTotal[fromCol][-1] == arrTotal[toCol][-1]:
+    def checkCompleted(self,col):
+        if len(set(self.arrTotal[col])) == 1 and len(self.arrTotal[col]) == self.m:
             return True
         else: return False
-    else: return False
 
-def moveBall(fromCol,toCol):
-    if validMove(fromCol,toCol):
-        num = arrTotal[fromCol].pop(-1)
-        arrTotal[toCol].append(num)
-        fillCompleted(toCol)
-    else:print("Invalid Move!")
+    def validMove(self,fromCol,toCol):
+        if len(self.arrTotal[fromCol]) > 0 and len(self.arrTotal[toCol]) < self.m and fromCol != toCol and not(self.completed[fromCol]):
+            if len(self.arrTotal[toCol]) == 0:
+                return True
+            elif self.arrTotal[fromCol][-1] == self.arrTotal[toCol][-1]:
+                return True
+            else: return False
+        else: return False
 
-def gameOver():
-    if completed.count(1) == n:
-        return True
-    else: return False
+    def moveBall(self,fromCol,toCol):
+        if self.validMove(fromCol,toCol):
+            num = self.arrTotal[fromCol].pop(-1)
+            self.arrTotal[toCol].append(num)
+            self.fillCompleted(toCol)
+        else:print("Invalid Move!")
+
+    def gameOver(self):
+        if self.completed.count(1) == self.n:
+            return True
+        else: return False
+
+    
 
 class Node:
     def __init__(self, matrix, arrCompleted,lastMove):
@@ -59,83 +66,107 @@ class Node:
         print("\nLast Move\n")
         print(self.lastMove)
 
-def drawMap():
-    screen.fill((236, 239, 241))
-    for x  in range(1,nTubes):
-        drawTube(x)
- 
-def color(number):
-    if number == 1:
-        color = (244, 67, 54)
-    elif number == 2:
-        color = (76, 175, 80)
-    elif number == 3:
-        color = (33, 150, 243)
-    elif number == 4:
-        color = (255, 152, 0)
-    elif number == 5:
-        color = (255, 193, 7)
-    elif number == 6:
-        color = (156, 39, 176)
-    return color
+class gameLoop:
+    def __init__(self):
+        self.game = Game(0)
+        self.tubeSelected = False
+        self.fromTube = -1
+        self.toTube = -1
+        self.currentLevel = 0
+  
+    def loadNextLevel(self):
+        self.currentLevel = self.currentLevel + 1 
+        self.game = Game(self.currentLevel)
+        self.tubeSelected = False
+        self.fromTube = -1
+        self.toTube = -1
 
-def drawTube(number):
-    center = SCREEN_WIDTH / nTubes
-    tube = pygame.Surface((60, 220))
-    tube.fill((38, 50, 56))
-    if number == fromTube:
-        tube.fill((144, 164, 174))
-    tube_center = (
-    (center*number -tube.get_width()/2),
-    (SCREEN_HEIGHT-tube.get_height())/2
-    )
-    pygame.draw.rect(tube,(236, 239, 241),pygame.Rect(5,0,50,215))
-    currentBall = 1
-    for x in arrTotal[number-1]:    
-        pygame.draw.circle(tube,color(x),(30,215-(25*currentBall)),20)
-        currentBall = currentBall +2
-    tubeClickable = screen.blit(tube, tube_center)
-    tubesArray[number-1] = tubeClickable
+    def drawMap(self):
+        screen.fill((236, 239, 241))
+        for x  in range(1,self.game.m+1):
+            self.drawTube(x)
+    
+    def color(self,number):
+        if number == 1:
+            color = (244, 67, 54)
+        elif number == 2:
+            color = (76, 175, 80)
+        elif number == 3:
+            color = (33, 150, 243)
+        elif number == 4:
+            color = (255, 152, 0)
+        elif number == 5:
+            color = (255, 193, 7)
+        elif number == 6:
+            color = (156, 39, 176)
+        return color
+
+    def drawTube(self,number):
+        center = SCREEN_WIDTH / (self.game.m+1)
+        tube = pygame.Surface((60, 220))
+        tube.fill((38, 50, 56))
+        if number == self.fromTube:
+            tube.fill((144, 164, 174))
+        tube_center = (
+        (center*number -tube.get_width()/2),
+        (SCREEN_HEIGHT-tube.get_height())/2
+        )
+        pygame.draw.rect(tube,(236, 239, 241),pygame.Rect(5,0,50,215))
+        currentBall = 1
+        for x in self.game.arrTotal[number-1]:    
+            pygame.draw.circle(tube,self.color(x),(30,215-(25*currentBall)),20)
+            currentBall = currentBall +2
+        tubeClickable = screen.blit(tube, tube_center)
+        self.game.tubesArray[number-1] = tubeClickable
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos=pygame.mouse.get_pos()
+                for tube in self.game.tubesArray:
+                    if tube.collidepoint(pos):
+                        if not self.tubeSelected : 
+                            self.fromTube = self.game.tubesArray.index(tube)+1
+                            self.tubeSelected = True
+                        else:
+                            self.toTube = self.game.tubesArray.index(tube)+1
+                            self.game.moveBall(self.fromTube-1,self.toTube-1)
+                            self.tubeSelected = False
+                            self.fromTube = -1 
+                            self.toTube = -1 
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return False
+            elif event.type == QUIT:
+                return False
+        return True
+
+    def update(self):
+        self.drawMap()
+        pygame.display.flip()
+        if self.game.gameOver():
+            if self.currentLevel < len(levels)-1:
+                self.loadNextLevel()
+            else:
+                return False
+        return True
+
 
 ## Game 
 pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-running = True
-tubeSelected = False
-fromTube = -1
-toTube = -1
-tubesArray = [0]*m
-nTubes = len(arrTotal)+1 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+gameLoop = gameLoop()
+
 
 # Main loop
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos=pygame.mouse.get_pos()
-            for tube in tubesArray:
-                if tube.collidepoint(pos):
-                    if not tubeSelected : 
-                        fromTube = tubesArray.index(tube)+1
-                        tubeSelected = True
-                    else:
-                        toTube = tubesArray.index(tube)+1
-                        moveBall(fromTube-1,toTube-1)
-                        tubeSelected = False
-                        fromTube = -1 
-                        toTube = -1 
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-        elif event.type == QUIT:
-            running = False
-    
-    drawMap()
-    pygame.display.flip()
-    if gameOver():
-        running= False
+    if not gameLoop.handleEvents():
+        running = False
+    if not gameLoop.update():
+        running = False
 pygame.quit()
 
 
