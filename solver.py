3e0e8fa4474b0558572467df24db17ec07c6a687
Muriel Pinho <muriel.apinho@gmail.com>
2021-04-03 02:17:23 +0100

    removed dead code

--- solver.py
+++ solver.py
@@ -1,25 +1,5 @@
 import copy 
 from collections import Counter
-from operator import itemgetter
-
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items)-1]
-
-    def size(self):
-        return len(self.items)
 
 class Node:
     """ Constructor """
